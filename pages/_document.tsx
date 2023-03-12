2e6af7bb9069aab00ad97d3b24fd7a90fb11514e
Youngchang Lee <apricotsoul@gmail.com>
2023-03-12 16:15:52 +0900

    fix: remove unnecessary code&lib in next-emotion-ts (#2)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,54 +1,15 @@
-// refer: https://github.com/ben-rogerson/twin.examples/blob/master/next-emotion-typescript/pages/_document.tsx
+import { Html, Head, Main, NextScript } from "next/document"
 
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-  DocumentInitialProps,
-} from 'next/document'
-import { extractCritical } from '@emotion/server'
-
-type NewDocumentInitialProps = DocumentInitialProps & {
-  ids: string[]
-  css: string
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
 
-class CustomDocument extends Document<NewDocumentInitialProps> {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx)
-    const critical = extractCritical(initialProps.html)
-    initialProps.html = critical.html
-    initialProps.styles = (
-      <>
-        {initialProps.styles}
-        <style
-          data-emotion-css={critical.ids.join(' ')}
-          dangerouslySetInnerHTML={{ __html: critical.css }}
-        />
-      </>
-    )
-
-    return initialProps
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <style
-            data-emotion-css={this.props?.ids?.join(' ')}
-            dangerouslySetInnerHTML={{ __html: this.props.css }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
-}
-
-export default CustomDocument
+export default Document
