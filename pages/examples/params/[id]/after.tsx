2bd121a4d34ef9351df5dfd9deaa488de99eb0d7
p-iknow <apricotsoul@gmail.com>
2023-03-12 22:55:27 +0900

    feat: after page

---
+++ pages/examples/params/[id]/after.tsx
@@ -0,0 +1,39 @@
+
+import { UrlQueryParams, useQueryParam } from '@/hooks';
+import { usePathParam } from '@/hooks/usePathParm';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/router';
+import { isArray, isString } from 'remeda';
+import tw from 'twin.macro';
+
+interface Props {
+  queryKey: string[] | null;
+}
+
+export const UseQueryParamPage:React.FC<Props> = () => {
+  const id = usePathParam('id');
+  const queryValue  = useQueryParam('queryKey') ?? ['default_queryKey_value']
+
+	return <div tw='flex justify-center items-center h-screen' >
+    <div css={tw`text-center m-auto`}>id: {id}<br />queryKey2: {queryValue}</div>
+  </div>
+}
+
+/** ServerSide Case */
+export const getServerSideProps: GetServerSideProps<Props, UrlQueryParams> = async (
+  {params} : GetServerSidePropsContext<UrlQueryParams>
+) => {
+
+  //use null, because JSON string don't have undefined type and It make error
+   const queryKey = params?.queryKey ?? null
+
+
+  return {
+    props: {
+      queryKey,
+      // Populate your component props here
+    },
+  };
+};
+
+export default UseQueryParamPage;
