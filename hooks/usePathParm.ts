afb9d65914d22dde47295e190163f57fdf5d079f
p-iknow <apricotsoul@gmail.com>
2023-03-12 18:11:20 +0900

    feat: usePathParam

---
+++ hooks/usePathParm.ts
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router';
+
+type ParamKeys =
+  | 'id'
+	| 'id2'
+
+
+/**
+* @description In the case of pathParam, there is no chance of it being undefined,
+* so we can cast it to a string type.
+*
+*/
+
+export const usePathParam = (paramKey: ParamKeys) => {
+  const { query } = useRouter();
+  return query[paramKey] as string;
+};
--- pages/examples/params/[id]/index.tsx
+++ pages/examples/params/[id]/index.tsx
@@ -1,14 +1,21 @@
 
 import { UrlQueryParams, useQueryParam } from '@/hooks';
+import { usePathParam } from '@/hooks/usePathParm';
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import tw from 'twin.macro';
 
 interface Props {
   queryKey: string | null;
 }
 
 export const UseQueryParamPage:React.FC<Props> = () => {
+
+  const id = usePathParam('id');
   const queryKey2  = useQueryParam('queryKey2') ?? 'default_queryKey2_value'
-	return <div>queryKey2: {queryKey2}</div>
+
+	return <div tw='flex justify-center items-center h-screen' >
+    <div css={tw`text-center m-auto`}>id: {id}<br />queryKey2: {queryKey2}</div>
+  </div>
 }
 
 /** ServerSide Case */
